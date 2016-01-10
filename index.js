b8ebf73b082b9ab7e84cb78443e804396d901074
FranciscoP <franciscopresencia@hotmail.com>
2016-01-10 20:59:03 +0100

    Updated code

--- index.js
+++ index.js
@@ -8,8 +8,23 @@ app.use(express.static(__dirname));
 
 // Routing
 io.on('connection', function (socket) {
+  
+  setInterval(function(){
+    var pos = {
+      left: parseInt(Math.random() * 100),
+      top: parseInt(Math.random() * 100)
+    };
+    console.log(pos);
+    
+    socket.emit('update', {
+      move: {
+        one: pos
+      }
+    });
+  }, 1000);
+  
   socket.on('click', function(data){
-    console.log('click', data);
+    //console.log('click', data);
   });
   socket.on('disconnect', function () { });
 });
--- javascript.js
+++ javascript.js
@@ -18,14 +18,3 @@ function update(data){
 }
 
 socket.on('update', update);
-
-setInterval(function(){
-  update({
-    move: {
-      one: {
-        left: parseInt(Math.random() * 100),
-        top: parseInt(Math.random() * 100)
-      }
-    }
-  });
-}, 1000);
