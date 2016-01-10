4bb9d601437bc3c1932c75b273a0ff90f2c04d72
Emilio Coppola <coppolaemilio@gmail.com>
2016-01-10 20:55:04 +0100

    grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,35 @@
 'use strict';
 
 module.exports = function(grunt) {
-grunt.initConfig({
-  sass: {
-    dist: {
-      files: {
-        './style/main.css': './style/main.scss'
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          './style/main.css': './style/main.scss'
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: {
+          './style/main.css': './style/main.css',
+        }
+      }
+    },
+    watch: {
+      style: {
+        files: ['./style/*.scss'],
+        tasks: ['default'],
       }
     }
-  }
-});
+  });
 
 
-grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-grunt.registerTask('default', ['sass']);
-
+  grunt.registerTask('default', ['sass', 'cssmin']);
 };
+
 }());
