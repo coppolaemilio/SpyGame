8522ac63ab901554f04a7784efa2d231e7d86095
FranciscoP <franciscopresencia@hotmail.com>
2016-01-10 19:52:12 +0100

    Added things'

--- javascript.js
+++ javascript.js
@@ -3,24 +3,33 @@ var socket = io();
 $("#scene").click(function(e){
   var pos = { x: event.pageX, y: event.pageY };
   
-  if (player.is('sniper')) {
-    socket.emit('shot', pos);
-  } else {
-    socket.emit('move', pos);
-  }
+  // if (player.is('sniper')) {
+  //   socket.emit('shot', pos);
+  // } else {
+  //   socket.emit('move', pos);
+  // }
 });
 
 function update(data){
   
-  console.log(data);
-  
   for(var key in data.move) {
     var move = data.move[key];
-    $("." + key).offset(move.x, move.y);
+    console.log(key);
+    $("." + key).offset(move);
   }
+  
+  $("player").each(function() {
+    $(this).css("z-index", $(this).css("top").replace("px",""));
+  });
 }
 
 
 socket.on('update', update);
 
-update({ move: { one: {x: 5, y: 10}}});
+setInterval(function(){
+  update({
+    move: {
+      one: { left: Math.random() * 100, top: Math.random() * 100 }
+    }
+  });
+}, 1000);
